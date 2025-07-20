7051a3676365ded57f328a9b635b9f796d05587f
Mukul Jain <81482724+MuKuLJain07@users.noreply.github.com>
2025-07-21 00:56:39 +0530

    a

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,58 @@
 const express = require('express');
+const path = require('path');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const app = express();
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../views'));
 
-app.get('', (request, response) => {
-    response.render('index');
-})
-app.get('/about', (request, response) => {
-    response.render('about');
-})
-app.get('/weather', (request, response) => {
-    response.render('weather', {
-        country: 'India',
-        temp: '32'
-    });
-})
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        });
+    }
+
+    geocode(req.query.address, (error, data) => {
+        if(error){
+            return console.log("Geocode Error: ", error);
+        }
+        
+        const latitude = data.latitude;
+        const longitude = data.longitude;
+        const location = data.location;
+        forecast(latitude, longitude, (error, data) => {
+            if(error){
+                return console.log("Forecast Error: ", error);
+            }
+            res.send({
+                Location: location,
+                Forecast: data,
+            });
+            // console.log("Location: ", location)
+            // console.log("Data: ", data);
+        })
+    })
+    // res.send({
+    //     Location: location,
+    //     Forecast: weather,
+    // })
+});
+
+// Catch-all route using RegEx
+app.all(/.*/, (request, response) => {
+    response.render('pageNotFound')
+});
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000');
-})
+});
