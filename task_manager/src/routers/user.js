c17f75545f095364dc6d2bcea401454f0912ebeb
Mukul Jain <81482724+MuKuLJain07@users.noreply.github.com>
2025-08-17 23:52:01 +0530

    updated task_manager

--- task_manager/src/routers/user.js
+++ task_manager/src/routers/user.js
@@ -22,6 +22,17 @@ router.post('/users', async (req, res) => {
     // })
 })
 
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
+        res.send(user);
+    } catch (e) {
+        console.error(e);
+        res.status(400).send({error: 'Login failed! Check authentication credentials.'});
+    }
+})
+
 router.get('/users', async (req, res) => {
 
     try {
@@ -73,14 +84,19 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true});
+        const user = await User.findById(_id);
+        updates.forEach((update) => {
+            user[update] = req.body[update];
+        });
+        await user.save();
+        // const user = await User.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true});
 
         if(!user) {
             return res.status(404).send();
         }
 
         res.send(user);
-    } catch {
+    } catch (e) {
         res.status(500).send(e);
     }
 })
@@ -102,3 +118,14 @@ router.delete('/users/:id', async (req, res) => {
 
 
 module.exports = router;
+
+
+
+const bcrypt = require('bcryptjs');
+const myfunc = async () => {
+    const password = 'Red12345!';
+    const hashedPassword = await bcrypt.hash(password, 8);
+    console.log(hashedPassword)  
+}
+
+myfunc()
